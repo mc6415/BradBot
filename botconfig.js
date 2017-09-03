94c4457cfe9681a3ccc96ad67e60e679ec11916a
Michael Coombes <mc6415@gmail.com>
2017-09-03 08:30:05 +0100

    Switched over from the obsolete jade to pug

---
+++ botconfig.js
@@ -0,0 +1,18 @@
+exports.config = {
+    "prefix": "-",
+    "discordToken": "MzUzNzUwNjM2OTM3ODcxMzYw.DI01kw.MW_pVMMfwO3OTh5qLlOjS76GeIY",
+    "sqlUser": "mcoombes",
+    "sqlPass": "Mc220391",
+    "sqlServer": "bradbot.database.windows.net",
+    "dbName": "BradBot"
+};
+
+exports.sqlConfig = {
+    userName: "mcoombes",
+    password: "Mc220391",
+    server: "bradbot.database.windows.net",
+    options: {
+        encrypt: true,
+        database: "BradBot"
+    }
+};
