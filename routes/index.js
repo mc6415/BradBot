97bb4cfefe4be9eb6b310e9001be9a0396e414cf
Michael Coombes <mc6415@gmail.com>
2017-09-05 04:23:56 +0100

    Testing theory

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const siteConfig = require('../botconfig.js');
-const Connection = require('tedious').Connection;
 const Request = require('tedious').Request;
 const TYPES = require('tedious').TYPES;
 const Theories = require('../saltytheory');
@@ -9,8 +8,8 @@ const siteService = require('../siteservice');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("Hitting Router");
-  res.render('index', { title: 'Express', isLoggedIn: siteService.isLoggedIn(req) });
+  res.render('index', {title: 'Express'});
+  // res.render('index', { title: 'Express', isLoggedIn: siteService.isLoggedIn(req) });
 });
 
 module.exports = router;
--- siteservice.js
+++ siteservice.js
@@ -1,3 +1,4 @@
 exports.isLoggedIn = (req) =>{
+    console.log("Problem Here");
     return !!req.session.user;
 };
